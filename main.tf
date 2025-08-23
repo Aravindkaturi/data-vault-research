0b6e9bb4b0eac8e14bddc7ca7cccfe5f70010bcc
Aravindkaturi <157713417+Aravindkaturi@users.noreply.github.com>
2025-08-23 11:37:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-provider "azurerm" {
-  features {}
 
-  use_oidc            = true
-  subscription_id     = var.subscription_id
-  tenant_id           = var.tenant_id
-  client_id           = var.client_id
-}
 # -------------------------------------------------
 # Backend Configuration (stores Terraform state in Azure)
 # -------------------------------------------------
