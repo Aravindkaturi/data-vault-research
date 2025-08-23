786fd7c5819a5f77c2aa04991e4487c688e23769
aravind <katuri.aravindh@gmail.com>
2025-08-23 15:44:58 +0530

    push 10

--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "owner_name" {
 variable "rg_name" {
   default = "rg-aravind-datavault"
 }
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
