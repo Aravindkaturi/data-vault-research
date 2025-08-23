2ce18d34a081e764bffd8ede922a4706504b3d72
aravind <katuri.aravindh@gmail.com>
2025-08-23 15:52:50 +0530

    push 11

--- providers.tf
+++ providers.tf
@@ -1,25 +1,8 @@
 provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-}
-
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.110.0"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
-
-  backend "azurerm" {
-    resource_group_name   = "rg-aravind-tfstate"
-    storage_account_name  = "stgaravindtfstate01"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
+  skip_provider_registration = true
 }
