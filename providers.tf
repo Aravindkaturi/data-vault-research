eacd73b1f0b4e434072205928e8706e2de83b592
Aravindkaturi <157713417+Aravindkaturi@users.noreply.github.com>
2025-08-23 11:39:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 3.110.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "rg-aravind-tfstate"
+    storage_account_name  = "stgaravindtfstate01"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -14,5 +20,10 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+
   skip_provider_registration = true
+  use_oidc        = true
+  subscription_id = "5b9a55fa-56e0-4847-8260-39eaa4ff49ca"
+  tenant_id       = "2e493aa5-02f1-4df8-8b03-4bca3f8d01e2"
+  client_id       = "a4c1933e-fcd0-4c01-9b17-22cf8b6b7d2a"
 }
